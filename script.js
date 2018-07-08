022542704638a365c101f02b9a0f5b0ca0d88002
Jason Rostar <jason.rostar@yahoo.com>
2018-07-08 08:52:14 -0700

    Concatenating Strings with Plus Operator

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
+

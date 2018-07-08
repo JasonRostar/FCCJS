f3eb3c5bcdcba8ebf605b8e35b0e199b12b0bc52
Jason Rostar <jason.rostar@yahoo.com>
2018-07-08 11:28:28 -0700

    Adding Variables to Strings

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Example
-var ourName = "freeCodeCamp";
-var ourStr = "Hello, our name is " + ourName + ", how are you?";
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
 
 // Only change code below this line
-var myName = "Jason";
-var myStr = "My name is " + myName + " and I am well!";
 
+var someAdjective = "great!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;

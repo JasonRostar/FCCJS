4868523ce195aa37f3330ba59c0688a17db1d127
Jason Rostar <jason.rostar@yahoo.com>
2018-07-08 15:58:52 -0700

    Modify Array Data With Indexes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
- // Example
- var ourArray = [50,60,70];
- var ourData = ourArray[0]; // equals 50
- 
- // Setup
- var myArray = [50,60,70];
- 
- // Only change code below this line.
- var myData = myArray[0];
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+myArray[0] = 45;

e0100a656b798eb334ccb5929388289227b96043
Jason Rostar <jason.rostar@yahoo.com>
2018-07-17 15:45:10 -0700

    Local Scope and Functions

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-// Declare your variable here
- 
-var myGlobal = 10;
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-  oopsGlobal = 5;
-}
-
-// Only change code above this line
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
+function myLocalScope(myVar) {
+    'use strict'; // you shouldn't need to edit this line
+    var myVar = "foo";
+    console.log(myVar);
   }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  
+  
+  // Now remove the console log line to pass the test

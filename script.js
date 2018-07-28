1f27a8a62c19bdf23bbc58b659b76f6fe6a64326
Jason Rostar <jason.rostar@yahoo.com>
2018-07-28 11:38:12 -0700

    Golf Code

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-function testSize(num) {
-    // Only change code below this line
-    if (num < 5) {
-      return ("Tiny");
-    } else if (num < 10) {
-      return ("Small");
-    } else if (num < 15) {
-      return ("Medium");
-    } else if (num < 20) {
-      return ("Large");
-    } else if (num >= 20) {
-      return ("Huge");
-    } else {
-      return "Change Me";
-    // Only change code above this line
-  }
-  };
-  // Change this value to test
-  testSize(7);
+
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (par >= 1 && strokes === 1) { //"Hole-in-one!"
+        return (names[0]); 
+ }  else if (par - 2 >= strokes) { //"Eagle"
+        return (names[1]); 
+ }  else if (par - 1 === strokes) { //"Birdie"
+        return (names[2]);
+ }  else if (par === strokes) { //"Par"
+        return (names[3]);
+ }  else if (par + 1 === strokes) { //"Bogey"
+        return (names[4]);
+ }  else if (par + 2 === strokes) { //"Double Bogey"
+        return (names[5]);
+ }  else { 
+    return (names[6]);
+ }
+  // Only change code above this line
+};
+
+// Change these values to test
+console.log(golfScore(5, 2));

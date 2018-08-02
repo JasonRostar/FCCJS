c0c886c085d95be16c31be85d170d5b1fed88aa5
Jason Rostar <jason.rostar@yahoo.com>
2018-08-01 18:29:23 -0700

    Return Early Pattern for Functions

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function isLess(a, b) {
-  // Fix this code
-  return a < b;
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a < 0 || b < 0) {
+return; } 
+  
+ 
+  
+  
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-// Change these values to test
-isLess(10, 15);
+// Change values below to test your code
+abTest(2,2);

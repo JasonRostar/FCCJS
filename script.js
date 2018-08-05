7cf94daef590971de6c3f989acc84bb1a22353b8
Jason Rostar <jason.rostar@yahoo.com>
2018-08-05 11:56:26 -0700

    Build JavaScript Objects

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-var count = 0;
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
 
-function cc(card) {
-  // Only change code below this line
-  switch(card){
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
-  }
-  if (count > 0){
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
-  // Only change code above this line
-}
+// Only change code below this line.
 
-console.log(cc('A'));
+var myDog = {
+  "name": "Bro",
+  "legs": 3,
+  "tails": 2,
+  "friends": ["Sport", "Champ"]
+};
